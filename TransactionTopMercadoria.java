895cc07f08e6e9e01454b5b6ab47959e7bad084a
brunoscapri <bruno.capriglioni@pucpr.edu.br>
2019-09-18 01:30:43 -0300

    questao 3

--- TransactionTopMercadoria.java
+++ TransactionTopMercadoria.java
@@ -1,8 +1,8 @@
 package advanced.customwritable;
 
+import advanced.customwritable.utils.MiscUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -14,10 +14,13 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.log4j.BasicConfigurator;
 
-import java.io.IOException;
-import java.util.StringTokenizer;
 
-public class Transaction {
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class TransactionTopMercadoria {
     public static void main(String args[]) throws IOException, ClassNotFoundException, InterruptedException {
         BasicConfigurator.configure();
 
@@ -33,10 +36,10 @@ public class Transaction {
         Job j = new Job(c, "TransactionCount");
 
         //declaracao das classes
-        j.setJarByClass(Transaction.class);
+        j.setJarByClass(TransactionTopMercadoria.class);
         j.setMapperClass(MapForTransaction.class);
         j.setReducerClass(ReduceForTransaction.class);
-        j.setCombinerClass(ReduceForTransaction.class);
+        j.setCombinerClass(CombinerForTransaction.class);
 
         //definicao dos tipos de saida
         j.setOutputKeyClass(Text.class);
@@ -51,7 +54,7 @@ public class Transaction {
         // lanca o job e aguarda sua execucao
         System.exit(j.waitForCompletion(true) ? 0 : 1);
     }
-    //entra              //sai
+                                                            //entra              //sai
     public static class MapForTransaction extends Mapper<LongWritable, Text, Text, IntWritable> {
 
         // Funcao de map
@@ -65,25 +68,20 @@ public class Transaction {
             //valores da lista
             String nomePais = valores[0].toLowerCase();
             String ano = valores[1];
+            String mercadoria = valores[3];
+
 
             if(nomePais.equals("brazil")){
-                con.write(new Text("Transacoes envolvendo Brasil "), new IntWritable(1));
-            }
-            //caso nao tenha nenhum valor com brazil
-            else{
-                con.write(new Text("Transacoes envolvendo Brasil "), new IntWritable(0));
-            }
-
-            //tirando a label de 'year' para nao contar como 1
-            if(!ano.equals("year")){
-                con.write(new Text(ano), new IntWritable(1));
-            }
-
+                if(ano.equals("2016")){
+                    con.write(new Text(mercadoria), new IntWritable(1));
+                }
+           }
         }
     }
 
     public static class ReduceForTransaction extends Reducer<Text, IntWritable, Text, IntWritable> {
 
+        private Map<Text, IntWritable> countMap = new HashMap<>();
         // Funcao de reduce
         public void reduce(Text word, Iterable<IntWritable> values, Context con)
                 throws IOException, InterruptedException {
@@ -91,8 +89,38 @@ public class Transaction {
             for(IntWritable v: values){
                 sum += v.get();
             }
-            con.write(word, new IntWritable(sum));
+            // ao inves de mandar direto para o output, o count map eh preenchido com os resultados
+            countMap.put(new Text(word), new IntWritable(sum));
 
+        }
+
+        //para que seja retornado algo ao output apos todos os dados coletados
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+
+            //cria um map ordenado com os elementos pelo valor
+            Map<Text, IntWritable> sortedMap = MiscUtils.sortByValues(countMap);
+
+            int counter = 0;
+            for (Text key : sortedMap.keySet()) {
+                //seleciona apenas os 5 primeiros elementos da lista
+                if (counter++ == 5) {
+                    break;
+                }
+                context.write(key, sortedMap.get(key));
+            }
+        }
+    }
+
+    public static class CombinerForTransaction extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            context.write(key, new IntWritable(sum));
         }
     }
 
